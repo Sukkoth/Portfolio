47e9444da2424651db63e04ed17228a773d31b9c
Sukkoth <suukootj@gmail.com>
2023-09-05 09:06:18 +0300

    config cors

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ const corsOptions = {
 app.use(express.static(__dirname + '/public'));
 
 app.use(morgan('dev'));
-app.use(cors(corsOptions));
+if (process.env.APP_ENV === 'DEV') {
+    app.use(cors(corsOptions));
+}
+
 app.use(express.json());
 app.get('/', (req, res) => {
     res.sendFile('index.html');
